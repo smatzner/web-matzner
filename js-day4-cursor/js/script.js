66192a00bcc8c13130c4830aaad21c7e1ee8eded
smatzner <stefanmatzner@gmx.at>
2023-09-01 11:52:46 +0200

    added mouseenter and mouseleave eventlisteners to links (task 5)

--- js-day4-cursor/js/script.js
+++ js-day4-cursor/js/script.js
@@ -1,6 +1,18 @@
 let cursor = document.getElementById('cursor');
+let links = document.querySelectorAll('a');
 
 document.addEventListener('mousemove', (e) => {
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
 });
+
+links.forEach((link) => {
+       link.addEventListener('mouseenter', (e) => {
+           cursor.classList.add('cursor-small');
+       });
+
+       link.addEventListener('mouseleave', (e) => {
+           cursor.classList.remove('cursor-small');
+       });
+});
+
