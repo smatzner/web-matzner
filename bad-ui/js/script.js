472eb864936c1d35a12d72413d3bf7ba1bcf4fd3
smatzner <stefanmatzner@gmx.at>
2023-09-04 13:09:29 +0200

    added functionality to exchange input fields when clicking on the change direction icon

--- bad-ui/js/script.js
+++ bad-ui/js/script.js
@@ -1,10 +1,24 @@
 let dateRangeElement = document.getElementById('dateRange');
 let dateSpan = document.getElementById('dateSpan');
+let changeDirection = document.getElementById('changeDirection');
+let bookingForm = document.getElementById('bookingForm');
 
 dateRangeElement.min = new Date()*1;
-dateRangeElement.value = new Date()+50;
+dateSpan.innerHTML = new Date().toLocaleString('de-AT',{dateStyle:"short",timeStyle:"short"});
+
+changeDirection.addEventListener('click', () => {
+    let fromDiv = document.getElementById('fromDiv');
+    let toDiv = document.getElementById('toDiv');
+    let fromDivClone = fromDiv.cloneNode(true);
+    let toDivClone = toDiv.cloneNode(true);
+
+    fromDiv.parentNode.replaceChild(toDivClone,fromDiv);
+    toDiv.parentNode.replaceChild(fromDivClone,toDiv);
+});
 
 dateRangeElement.addEventListener('input', (e) => {
     let selectedDate = new Date(dateRangeElement.value*1);
     dateSpan.innerHTML = selectedDate.toLocaleString('de-AT',{dateStyle:"short",timeStyle:"short"});
 });
+
+console.log(new Date(2025,12)*1)
