c91cb8d98b785012f541e0337c3170be85b7eba2
smatzner <stefanmatzner@gmx.at>
2023-09-14 23:14:30 +0200

    added lightModeButton // fixed rickroll to work on every viewport // move login.html into index.html and toggle with JS // added closeCookies function to x-btn

--- bad-ui/js/script.js
+++ bad-ui/js/script.js
@@ -1,6 +1,12 @@
 let siteLoadOverlay = document.getElementById('siteLoadOverlay');
+let logo = document.getElementById('navbarBrand');
+let loginLink = document.getElementById('loginLink');
+let loginParagraph = document.getElementById('loginParagraph');
+let index = document.getElementById('index');
+let login = document.getElementById('login');
+let loginForm = document.getElementById('loginForm');
 let container = document.getElementById('container');
-let closeCookies = document.getElementById('closeCookies');
+let closeCookies = document.querySelectorAll('.closeCookies');
 let acceptCookies = document.getElementById('acceptCookies');
 let modal = document.querySelector('.modal');
 let modalWindow = document.getElementById('modalWindow');
@@ -18,9 +24,9 @@ let rickRollLinkRect = document.getElementById('rickRollLink').getBoundingClient
 let darkModeButton = document.getElementById('darkModeButton');
 let darkModeOverlay = document.getElementById('darkModeOverlay');
 let realDarkModeButton = document.getElementById('realDarkModeButton');
-let realDarkModeOverlay = document.getElementById('realDarkModeOverlay');
 let realDarkModeCursor = document.getElementById('realDarkModeCursor');
 let realDarkMode = false;
+let lightModeButton = document.getElementById('lightModeButton');
 let labels = document.querySelectorAll("label");
 
 
@@ -33,6 +39,7 @@ if (localStorage['guest'] === 'rude') {
     container.innerHTML = '<h1>Rude Guests are not welcome here. Please try a different railway company.</h1>';
 }
 
+// TODO entfernen
 if (localStorage['siteLoaded'] !== 'true') {
     siteLoadOverlay.classList.remove('d-none');
     document.addEventListener('dblclick', startSite);
@@ -57,22 +64,52 @@ window.addEventListener('mousemove', (e) => {
 });
 
 window.addEventListener('click', (e) => {
-    cursorSize *= 1.5;
-    cursor.style.width = cursorSize + 'px';
-    cursor.style.height = cursorSize + 'px';
+    if(rickRoll){
+        cursorSize *= 1.3;
+        cursor.style.width = cursorSize + 'px';
+        cursor.style.height = cursorSize + 'px';
+    }
 
     let cursorRect = cursor.getBoundingClientRect();
 
-    if ((cursorRect.top >= 350 && cursorRect.top <= 370) && (cursorRect.left >= 485 && cursorRect.left <= 630)) {
+    let minX = rickRollLinkRect.top - 100;
+    let maxX = rickRollLinkRect.top + 100;
+    let minY = rickRollLinkRect.left - 100;
+    let maxY = rickRollLinkRect.left + 100;
+    if ((cursorRect.top >= minX && cursorRect.top <= maxX) && (cursorRect.left >= minY && cursorRect.left <= maxY)) {
         window.open('https://youtu.be/xvFZjo5PgG0?si=KryHi8U8OMf9AutX', '_blank');
     }
 });
 
-/* --- cookies --- */
-closeCookies.addEventListener('click', () => {
-    document.body.innerHTML = '<h1>Rude Guests are not welcome here. Please try a different railway company.</h1>';
-    localStorage['guest'] = 'rude';
+/* --- switch pages --- */
+loginLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    index.classList.toggle('d-none');
+    login.classList.toggle('d-none');
+})
+
+logo.addEventListener('click', (e) => {
+    e.preventDefault();
+    index.classList.remove('d-none');
+    login.classList.add('d-none');
+})
+
+/* --- login --- */
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    index.classList.toggle('d-none');
+    login.classList.toggle('d-none');
+    loginParagraph.classList.add('d-none');
 });
+
+/* --- cookies --- */
+closeCookies.forEach(closeCookie => {
+    closeCookie.addEventListener('click', () => {
+        container.innerHTML = '<h1>Rude Guests are not welcome here. Please try a different railway company.</h1>';
+        localStorage['guest'] = 'rude';
+    });
+})
+
 
 let cookiesEventCounter = 0;
 acceptCookies.addEventListener('mousemove', cookiesMouseMoveHandler);
@@ -81,7 +118,7 @@ acceptCookies.addEventListener('click', () => {
     modal.classList.remove('d-block');
 });
 
-/* --- dark mode event --- */
+/* --- dark mode events --- */
 darkModeButton.addEventListener('click', (e) => {
     darkModeOverlay.classList.remove('d-none');
     darkModeButton.classList.add('d-none');
@@ -90,9 +127,12 @@ darkModeButton.addEventListener('click', (e) => {
 
 realDarkModeButton.addEventListener('click', (e) => {
     e.stopPropagation();
+
+    darkModeOverlay.classList.add('d-none');
+
     realDarkMode = true;
-    realDarkModeOverlay.classList.remove("d-none");
-    realDarkModeButton.classList.add("d-none");
+    realDarkModeCursor.classList.remove('d-none');
+    realDarkModeButton.classList.add('d-none');
 
     cursor.classList.add('d-none');
     labels.forEach((label) => {
@@ -101,9 +141,27 @@ realDarkModeButton.addEventListener('click', (e) => {
 
     document.body.style.overflow = 'hidden';
 
-    realDarkModeCursor.style.left = e.clientX + 'px';
-    realDarkModeCursor.style.top = e.clientY + 'px';
+    let clientX = e.clientX - 4040;
+    let clientY = e.clientY - 4040;
+    realDarkModeCursor.style.left = clientX + 'px';
+    realDarkModeCursor.style.top = clientY + 'px';
+
+    setTimeout(() => {
+        lightModeButton.classList.remove('d-none');
+        lightModeButton.style.left = Math.floor(Math.random() * 1000) + 'px';
+    },1000);
 });
+
+lightModeButton.addEventListener('click', () => {
+    darkModeButton.classList.remove('d-none');
+    lightModeButton.classList.add('d-none');
+    realDarkModeCursor.classList.add('d-none');
+    cursor.classList.remove('d-none');
+    realDarkMode = false;
+});
+
+/* --- form --- */
+bookingForm.addEventListener('submit', e => e.preventDefault());
 
 changeDirection.addEventListener('click', () => {
     let fromDiv = document.getElementById('fromDiv');
@@ -161,7 +219,7 @@ function submitButtonEventHandler(e) {
         rickRoll = false;
     }, 1000)
 
-    if (submitButtonEventCounter === 2) {
+    if (submitButtonEventCounter === 5) {
         submitButton.removeEventListener('mouseenter', submitButtonEventHandler);
     }
 }
