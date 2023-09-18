49404bab87dd3a97cf686dec45786cb3a219bf01
smatzner <stefanmatzner@gmx.at>
2023-09-18 13:27:26 +0200

    set up html structure (task 1)

--- vue-day1-verwaltung/src/App.vue
+++ vue-day1-verwaltung/src/App.vue
@@ -1,47 +1,52 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <h1 class="my-5">Übung: Personen Array</h1>
+    <div class="row">
+      <div class="col-12 col-md-5">
+        <div class="p-4 bg-light">
+          <h2 class="mb-4">Person anlegen</h2>
+          <form id="addPersonForm" class="mb-5">
+            <div class="mb-3">
+              <label class="form-label" for="addPersonFirstName">Vorname</label>
+              <input type="text" name="firstName" class="form-control" id="addPersonFirstName" required>
+            </div>
+            <div class="mb-3">
+              <label class="form-label" for="addPersonLastName">Nachname</label>
+              <input type="text" name="lastName" class="form-control" id="addPersonLastName" required>
+            </div>
+            <div class="mb-3">
+              <label class="form-label" for="addPersonBirthYear">Geburtsjahr</label>
+              <input type="number" name="birthYear" class="form-control" id="addPersonBirthYear" required>
+            </div>
+            <button type="submit" class="btn btn-success">Hinzufügen</button>
+          </form>
+          <h2 class="mb-4">Aktionen</h2>
+          <div class="d-flex gap-3">
+            <button type="button" class="btn btn-danger" id="deletePersonButton">Letztes Element löschen</button>
+            <button type="button" class="btn btn-danger" id="clearAllButton">Alles löschen</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-7">
+        <table class="table table-striped">
+          <thead>
+          <tr>
+            <th>Vorname</th>
+            <th>Nachname</th>
+            <th>Alter</th>
+          </tr>
+          </thead>
+          <tbody id="personTableBody"></tbody>
+        </table>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
