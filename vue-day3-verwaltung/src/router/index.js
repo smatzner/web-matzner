36a8d31a5a0a2fce1993c487d1574ec2abb30838
smatzner <stefanmatzner@gmx.at>
2023-10-02 13:58:16 +0200

    added DetailView Router

--- vue-day3-verwaltung/src/router/index.js
+++ vue-day3-verwaltung/src/router/index.js
@@ -1,16 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import CreateView from "@/views/CreateView.vue";
+import DetailView from "@/views/DetailView.vue";
 
 const routes = [
-    {
-        path: '/',
-        component: HomeView
-    },
-    {
-        path: '/create',
-        component: CreateView
-    }
+    {path: '/', component: HomeView},
+    {path: '/create', component: CreateView},
+    {path: '/persons/:id', component: DetailView}
 ]
 
 const router = createRouter({
@@ -18,4 +14,4 @@ const router = createRouter({
     history: createWebHistory()
 })
 
-export{router}
+export {router}
