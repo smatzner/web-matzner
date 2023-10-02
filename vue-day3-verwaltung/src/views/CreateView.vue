7c1077554ee276d5a70f7e6e92afcca8ecc42b5c
smatzner <stefanmatzner@gmx.at>
2023-10-02 13:42:06 +0200

    implemented addPerson function to save new persons to personStore (task 2.5)

--- vue-day3-verwaltung/src/views/CreateView.vue
+++ vue-day3-verwaltung/src/views/CreateView.vue
@@ -1,8 +1,20 @@
 <script setup>
 import PersonForm from "@/components/PersonForm.vue";
+import {usePersonStore} from "@/store/PersonStore";
+import {router} from "@/router";
+
+const personStore = usePersonStore()
 
 function addPerson(person) {
   console.log('Person hinzugefügt')
+  const id = Math.round(Math.random() * 100000)
+  personStore.persons.push({
+    id,
+    firstName: person.firstName,
+    lastName: person.lastName,
+    birthYear: person.birthYear
+  })
+  router.push('/')
 }
 </script>
 
