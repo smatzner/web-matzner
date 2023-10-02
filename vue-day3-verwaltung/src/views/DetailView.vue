8675d95f9e83f6d22d668f5e95ac608d409478cd
smatzner <stefanmatzner@gmx.at>
2023-10-02 13:59:36 +0200

    added id to read out id in detailview path (task 3.3)

--- vue-day3-verwaltung/src/views/DetailView.vue
+++ vue-day3-verwaltung/src/views/DetailView.vue
@@ -1,9 +1,14 @@
 <script setup>
+import {useRoute} from "vue-router";
+import {computed} from "vue";
 
+const id = computed(() => useRoute().params.id)
+
+console.log(id.value)
 </script>
 
 <template>
-
+  <div>Details</div>
 </template>
 
 <style scoped>
