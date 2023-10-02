375c180f0b11951fc9cdff34e09a9af5694fb844
smatzner <stefanmatzner@gmx.at>
2023-10-02 13:40:26 +0200

    changed PersonStore from option store to setup store

--- vue-day3-verwaltung/src/store/PersonStore.js
+++ vue-day3-verwaltung/src/store/PersonStore.js
@@ -1,12 +1,27 @@
 import {defineStore} from "pinia";
+import {ref} from "vue";
 
 
-export const usePersonStore = defineStore('person',{
-    state: () => ({
-        persons: [{
+// export const usePersonStore = defineStore('person',{
+//     state: () => ({
+//         persons: [{
+//             id: 1,
+//             firstName: 'Max',
+//             lastName: 'Mustermann',
+//             birthYear: 1999
+//         }]
+//     })
+// })
+
+export const usePersonStore = defineStore('person', () => {
+    const persons = ref([
+        {
+            id: 1,
             firstName: 'Max',
             lastName: 'Mustermann',
             birthYear: 1999
-        }]
-    })
+        }
+    ])
+
+    return {persons}
 })
